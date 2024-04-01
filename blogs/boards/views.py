ec51ad12f0b6188262462324eb989a5e5e92e0ec
Ruman Dangol <rumancha12@gmail.com>
2024-04-01 11:46:46 +0545

    New URL added for about page and topic list page also create a super admin user and added new board through admin portal

--- blogs/boards/views.py
+++ blogs/boards/views.py
@@ -15,3 +15,12 @@ def home(request):
 
     # return HttpResponse(response_html)
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    # return HttpResponse("List of board topics")
+    board = Board.objects.get(pk=pk)
+    return render(request, 'topics.html', {'board': board})
+
+def about(request):
+    # do something...
+    return render(request, 'about.html')
